c0aeded73da6bfb57714b719d38b10d3638328e5
shdrfq <shahidrafiq06@gmail.com>
2024-08-18 11:14:13 +0000

    change in veriable file

--- veriables.py
+++ veriables.py
@@ -2,3 +2,23 @@ x = 5
 y = "Shahid"
 print(x)
 print(y)
+
+
+#=============CLASS 18.08.24====
+
+fname:str = "Shahid"
+lname:str = "rafiq"
+fullname:str = fname+lname
+print (fullname)
+#------------value over write when we assingn 
+
+#----------Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#----------MOne Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
