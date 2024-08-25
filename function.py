45453c5edec73f687e6d532e59c08d9fa7d35796
shdrfq <shahidrafiq06@gmail.com>
2024-08-25 11:38:01 +0000

    change in funtion file

--- function.py
+++ function.py
@@ -2,6 +2,18 @@ def my_function():
  num1:int = 10
  num2:int = 20
  result:int = num1+num2
- print(result)
+ #  print(result)
+ return(result)
+my_function()
+#---------------------------
+def my_function2(fname, lname):
+  print(fname + " " + lname)
 
+my_function2("shahid","rafiq")
+#---------------------------
+"""
+afdadfsaf
+sdfdsafasd
+sdfdsa
 
+"""
